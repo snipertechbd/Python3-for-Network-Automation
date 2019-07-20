7899338d46a4b98343fe467c0cc1b18c3d4c9d89
SniperTech Bangladesh <53123418+snipertechbd@users.noreply.github.com>
2019-07-20 23:16:10 +0600

    Update and rename Config pulling.py to Cisco Config pulling_telnet.py

--- Cisco Config pulling_telnet.py
+++ Cisco Config pulling_telnet.py
@@ -1,6 +1,6 @@
 
 #!/user/bin/env python3
-#Running config
+#Cisco Config pulling using telnet
 
 import getpass
 import telnetlib
@@ -8,8 +8,10 @@ import telnetlib
 user = input("Enter your Telnet Username: ")
 password = getpass.getpass()
 
+#Opening a file where we saved devives IP information
 f = open("Mydevices")
 
+#Looping to get access to all the devicess and pull informations
 for IP in f:
 	IP = IP.strip()
     print ("Pulling Running Config" + (IP))
@@ -21,7 +23,7 @@ for IP in f:
 	if password:
 		tn.read_until(b"Password: ")
     	tn.write(password.encode('ascii') + b"\n")
-
+#Cisco Commands
     tn.write(b"terminal length 0\n")
     tn.write(b"show envirnment\n")
     tn.write(b"show inventory\n")
@@ -30,10 +32,11 @@ for IP in f:
     tn.write(b"show ip int bri\n")
     tn.write(b"exit\n")
 
-
+#Saving output in a new file for each 
     readoutput = tn.read_all()
     saveoutput = open("TEST_Device_" + HOST, "w")
     saveoutput.write(readoutput.decode("ascii"))
     saveoutput.write("\n")
     saveoutput.close
+#Print output in cli of Network Automation Container
     print(tn.read_all().decode('ascii'))
